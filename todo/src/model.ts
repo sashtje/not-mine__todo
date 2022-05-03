529b1204c57a022d806fa31e01126ab99edd9031
sashtje <sashtje.lefevre@gmail.com>
2022-05-03 15:31:01 +0300

    feat: add todo project

---
+++ todo/src/model.ts
@@ -0,0 +1,14 @@
+export interface Todo {
+  id: number;
+  todo: string;
+  isDone: boolean;
+}
+
+/* import { useReducer } from "react";
+
+const ReducerExample = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+
+  return <div></div>;
+};
+ */
